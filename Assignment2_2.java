dcee87d6aa8476bf4ddfa14e09267603b045a65b
rajanku2512 <rajanku2512@gmail.com>
2017-01-10 01:19:24 +0530

    Add files via upload

---
+++ Assignment2_2.java
@@ -0,0 +1,58 @@
+
+public class Assignment2_2 {
+
+	public static void main(String[] args) {
+		// Write a program to print a necklace pattern:
+	
+		
+		for(int j=1;j<=4;j++){
+			switch (j) {
+			case 1:
+			
+			
+		for(int i=1;i<=7;i++){
+			if(i==1 || i==7){
+			System.out.print("*");}
+			else{
+				System.out.print(" ");
+				}
+		}		System.out.println(" ");
+			
+			case 2:
+				for(int i=1;i<=7;i++){
+				if(i==2 || i==6){
+					System.out.print("*");}
+					else{
+						System.out.print(" ");
+						}
+				}		System.out.println(" ");
+			case 3:for(int i=1;i<=7;i++){
+				if(i==3 || i==5){
+					System.out.print("*");}
+					else{
+						System.out.print(" ");
+						}
+				}		System.out.println(" ");
+			case 4:
+				for(int i=1;i<=7;i++){
+					if(i==4 ){
+					System.out.print("*");}
+					else{
+						System.out.print(" ");
+						}
+				}		System.out.println(" ");}
+		
+		
+		
+		
+		}
+		
+		
+		
+			}
+	}	
+
+			
+	
+
+
